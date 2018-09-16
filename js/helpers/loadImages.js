ae3979c2cae32efcf45c8bfb0df1cb34968381aa
elyft <kishigo8@gmail.com>
2018-09-16 17:48:42 +0100

    Some Adjustements

--- js/helpers/loadImages.js
+++ js/helpers/loadImages.js
@@ -1,32 +1,29 @@
 export function loadImages(...imagesPaths){
   return new Promise((resolve)=>{
-    handleAllCheckIfImageLoadedOrNot(...imagesPaths);
-  })
- //  return Promise.all(arrayOfImagesPath.map(checkIfImageLoadedOrNot)).then(function(values) {
- //  console.log(values);
- // });
-}
-function checkIfImageLoadedOrNot(imagePath){
-   return new Promise((resolve)=>{
-     const img = new Image();
-     img.src = imagePath;
-     img.onload = ()=>{
-       if(img.complete === true){
-         console.log("loaded");
-         resolve({imagePath, status:'fully loaded'});
-       }else{
-         resolve({imagePath, status:'not completelyloaded'});
-       }
-     };
-     img.onerror = () => {
-       console.log("error");
-       resolve({imagePath, status: 'error'});
-     }
-   });
-}
+    function checkIfImageLoadedOrNot(imagePath){
+       return new Promise((resolve)=>{
+         const img = new Image();
+         img.src = imagePath;
+         img.onload = ()=>{
+           if(img.complete === true){
+             // For Debugging purposes
+             resolve({imagePath, status:'fully loaded'});
+           }else{
+             // For Debugging purposes
+             resolve({imagePath, status:'not completelyloaded'});
+           }
+         };
+         img.onerror = () => {
+           resolve({imagePath, status: 'error'});
+         }
+       });
+    }
 
-function handleAllCheckIfImageLoadedOrNot(...imagesPaths){
-  return Promise.all(imagesPaths.map(checkIfImageLoadedOrNot)).then((values)=>{
-    console.log(values);
+    function handleAllCheckIfImageLoadedOrNot(...imagesPaths){
+      return Promise.all(imagesPaths.map(checkIfImageLoadedOrNot)).then((values)=>{
+        resolve("All Images are loaded")
+      });
+    }
+    handleAllCheckIfImageLoadedOrNot(...imagesPaths);
   })
 }
--- js/index.js
+++ js/index.js
@@ -5,8 +5,14 @@ import {secondViewSlide} from './modules/secondviewSlide.js';
 // import {thirdViewSlide} from './modules/thirdviewslide.js';
 document.addEventListener("DOMContentLoaded", () =>{
   "use strict";
-  loadImages(...['img/bg1.jpg','img/bg2.jpg','img/bg3.jpg']);
-  // carroussel(document.querySelectorAll(".carroussel-elements"));
+  loadImages(...['img/bg1.jpg','img/bg2.jpg','img/bg3.jpg'])
+  .then((response)=>{
+     if(response === "All Images are loaded"){
+       carroussel(document.querySelectorAll(".carroussel-elements"));
+     }
+  }).catch((error)=>{
+    console.log(error);
+  })
   firstViewSlide();
   secondViewSlide();
 },{passive:true,capture:false});
