ef7712e84dfe4198050773b465da194624596876
elyft <kishigo8@gmail.com>
2018-09-16 09:49:05 +0100

    Some Updates

--- js/helpers/loadImages.js
+++ js/helpers/loadImages.js
@@ -1,23 +1,32 @@
-export function loadImages(...arrayOfImagesPath){
-  const checkIfImageLoadedOrNot = (imagePath)=>{
-    new Promise((resolve)=>{
-      const img = new Image();
-      img.src = imagePath;
-      img.onload = ()=>{
-        if(img.complete === true){
-          console.log("loaded");
-          resolve({imagePath, status:'fully loaded'});
-        }else{
-          resolve({imagePath, status:'not completelyloaded'});
-        }
-      };
-      img.onerror = () => {
-        console.log("error");
-        resolve({imagePath, status: 'error'});
-      }
+export function loadImages(...imagesPaths){
+  return new Promise((resolve)=>{
+    handleAllCheckIfImageLoadedOrNot(...imagesPaths);
+  })
+ //  return Promise.all(arrayOfImagesPath.map(checkIfImageLoadedOrNot)).then(function(values) {
+ //  console.log(values);
+ // });
+}
+function checkIfImageLoadedOrNot(imagePath){
+   return new Promise((resolve)=>{
+     const img = new Image();
+     img.src = imagePath;
+     img.onload = ()=>{
+       if(img.complete === true){
+         console.log("loaded");
+         resolve({imagePath, status:'fully loaded'});
+       }else{
+         resolve({imagePath, status:'not completelyloaded'});
+       }
+     };
+     img.onerror = () => {
+       console.log("error");
+       resolve({imagePath, status: 'error'});
+     }
    });
-  }
-  return Promise.all(arrayOfImagesPath.map(checkIfImageLoadedOrNot)).then(function(values) {
-  console.log(values);
- });
+}
+
+function handleAllCheckIfImageLoadedOrNot(...imagesPaths){
+  return Promise.all(imagesPaths.forEach(checkIfImageLoadedOrNot)).then((values)=>{
+    console.log(values);
+  })
 }
