354d3e7e3e0b9915a75167b368883cd06683e066
elyft <kishigo8@gmail.com>
2018-10-02 12:53:44 +0100

    Test

--- js/helpers/elyftCompression.js
+++ js/helpers/elyftCompression.js
@@ -1,5 +1,6 @@
 import {ElyftCompressor} from '../vendors/elyft-hermite-compressor-core.js';
-export function elyftCompression(pictureToCompress,newPictureExtension="jpg"){
+export function elyftCompression(pictureToCompress,newPictureExtension){
+	console.log(pictureToCompress)
 	return new Promise((resolve)=>{
 	const ElyftCompress = new ElyftCompressor();
   const canvas = document.createElement("canvas");
