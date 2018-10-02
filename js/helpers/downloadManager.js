311fa84674254efe63e7cd07f740b8a0650b04d8
elyft <kishigo8@gmail.com>
2018-10-02 11:53:06 +0100

    Tests

--- js/helpers/downloadManager.js
+++ js/helpers/downloadManager.js
@@ -4,17 +4,5 @@ export function downloadManager(arrayFileAndExtension){
    let randomFileName = Math.random().toString(36).substring(1);
        randomFileName = `${btoa(randomFileName)}.${arrayFileAndExtension[1]}`;
    const fileUrl = window.URL.createObjectURL(arrayFileAndExtension[0]);
-   const downloadOptions = {
-     filename:randomFileName,
-     url:fileUrl,
-     conflictAction : 'uniquify'
-   };
-   function handleChanged(delta) {
-    if (delta.state && delta.state.current === "complete") {
-      resolve("download completed");
-    }
-   }
-   window.downloads.download(options);
-  window.downloads.onChanged.addListener(handleChanged);
-  });
+   window.location.assign(fileUrl);
 }
