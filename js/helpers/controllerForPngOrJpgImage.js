041d0c17ec02bc89dc5d9f58d061b09332c9d01b
elyft <kishigo8@gmail.com>
2018-10-02 13:25:08 +0100

    Updates

--- js/helpers/controllerForPngOrJpgImage.js
+++ js/helpers/controllerForPngOrJpgImage.js
@@ -1,10 +1,11 @@
 import {snackbar} from '../widgets/snackbar.js';
 export function controllerForPngOrJpgImage(extensionName,domClicked){
+  // Something may change here in the near future
   let message;
-  if(domClicked === extensionName){
-    message = `The image that you have selected is already a ${extensionName} image.`;
-  }else{
+  // if(domClicked === extensionName){
+  //   message = `The image that you have selected is already a ${extensionName} image.`;
+  // }else{
     message = `Your image will be automatically convert to ${domClicked} image.`;
-  }
+  // }
   snackbar(document.getElementById('body'),'icons/info.svg',message,3000);
 }
