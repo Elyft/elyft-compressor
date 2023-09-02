19f5eef59deda42fb62d574b2b83fb253ac978b7
UBIX-GROUP <42975253+Elyft@users.noreply.github.com>
2023-09-02 13:21:09 +0100

    Remove some comments and organize the code a liitle bit

--- js/helpers/controllerForPngOrJpgImage.js
+++ js/helpers/controllerForPngOrJpgImage.js
@@ -1,11 +1,6 @@
 import {snackbar} from '../widgets/snackbar.js';
 export function controllerForPngOrJpgImage(extensionName,domClicked){
   // Something may change here in the near future
-  let message;
-  // if(domClicked === extensionName){
-  //   message = `The image that you have selected is already a ${extensionName} image.`;
-  // }else{
-    message = `Your image will be automatically convert to ${domClicked} image.`;
-  // }
+  let message = `Your image will be automatically convert to ${domClicked} image.`;
   snackbar(document.getElementById('body'),'icons/info.svg',message,2000);
 }
